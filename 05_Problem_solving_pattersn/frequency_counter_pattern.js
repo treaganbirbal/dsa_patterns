d9fcc575aa7dd593164e23bf51601f481f8aefcf
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-28 15:51:44 -0500

    quadratic approach to same implmentation

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -1,12 +1,22 @@
 //write a function called same that accepts 2 arrays.The function should return true if every value in first arrray has its corresponding squared value in the second array.  The frequency of value must remain the same.
 
 function same(array1, array2){
-    return false;
+    if(array1.length !== array2.length){
+        return false;
+    }
+    for(let i = 0; i < array1.length; i++){
+        let correctIndex = array2.indexOf(array1[i]**2)
+        if(correctIndex === -1){
+            return false;
+        }
+        array1.splice(correctIndex, 1)
+    }
+    return true;
 }
 
-let arr1 = [1, 2, 3, 4]
-let arr2 = [1, 4, 9, 16]
+let arr1 = [2, 2, 3, 4]
+let arr2 = [4, 9, 4, 16]
 let arr3 = [1, 4, 5, 16]
-let arr4 = [1, 4, 9, 16, 17];
+let arr4 = [4, 4, 9, 16, 17];
 
 console.log(same(arr1, arr2));
