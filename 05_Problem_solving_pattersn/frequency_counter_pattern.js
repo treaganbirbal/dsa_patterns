0e4dba59bd371df89af04a545a9f3fe083241065
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-28 14:20:12 -0500

    start implementation of same

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -1,2 +1,12 @@
 //write a function called same that accepts 2 arrays.The function should return true if every value in first arrray has its corresponding squared value in the second array.  The frequency of value must remain the same.
 
+function same(array1, array2){
+    return false;
+}
+
+let arr1 = [1, 2, 3, 4]
+let arr2 = [1, 4, 9, 16]
+let arr3 = [1, 4, 5, 16]
+let arr4 = [1, 4, 9, 16, 17];
+
+console.log(same(arr1, arr2));
