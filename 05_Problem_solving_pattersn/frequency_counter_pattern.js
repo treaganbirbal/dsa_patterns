1199baa7d6e397d0dab87685375a77270adfa2af
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-28 16:36:39 -0500

    linear implementation of same function

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -1,15 +1,42 @@
 //write a function called same that accepts 2 arrays.The function should return true if every value in first arrray has its corresponding squared value in the second array.  The frequency of value must remain the same.
+//------naive approach
+// function same(array1, array2){
+//     if(array1.length !== array2.length){
+//         return false;
+//     }
+//     for(let i = 0; i < array1.length; i++){
+//         let correctIndex = array2.indexOf(array1[i]**2)
+//         if(correctIndex === -1){
+//             return false;
+//         }
+//         array1.splice(correctIndex, 1)
+//     }
+//     return true;
+// }
+
+//---------o(n)
 
 function same(array1, array2){
     if(array1.length !== array2.length){
         return false;
     }
-    for(let i = 0; i < array1.length; i++){
-        let correctIndex = array2.indexOf(array1[i]**2)
-        if(correctIndex === -1){
+    let arr1Map = {};
+    let arr2Map = {};
+    for(let values of array1){
+        arr1Map[values] = (arr1Map[values] || 0) + 1;
+    }
+    for(let values of array2){
+        arr2Map[values] = (arr2Map[values] || 0) + 1;
+    }
+    console.log(arr1Map)
+    console.log(arr2Map)
+    for(let key in arr1Map ){
+        if(!(key ** 2 in arr2Map)){
             return false;
         }
-        array1.splice(correctIndex, 1)
+        if(arr2Map[key ** 2] !== arr1Map[key]){
+            return false
+        }
     }
     return true;
 }
