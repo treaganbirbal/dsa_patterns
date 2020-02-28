7e26e763749b63e4687752aae15da384d2b32656
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-28 14:17:54 -0500

    same problem prompt

---
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -0,0 +1,2 @@
+//write a function called same that accepts 2 arrays.The function should return true if every value in first arrray has its corresponding squared value in the second array.  The frequency of value must remain the same.
+
