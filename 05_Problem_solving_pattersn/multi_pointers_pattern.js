e4406617225db3e67538b0b50ac9616579ff1e84
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-01 16:44:34 -0500

    prompt for sumZero function

--- 05_Problem_solving_pattersn/multi_pointers_pattern.js
+++ 05_Problem_solving_pattersn/multi_pointers_pattern.js
@@ -1,1 +1,3 @@
+//Write a function called sumZero which accepts a sorted array of integers.  the function shoudl find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
 
