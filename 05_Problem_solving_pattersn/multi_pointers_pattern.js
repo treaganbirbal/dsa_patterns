82679754c9f91ebb87971c9970a518eecdd801f9
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-01 18:27:58 -0500

    prompt for countUniqueValues

--- 05_Problem_solving_pattersn/multi_pointers_pattern.js
+++ 05_Problem_solving_pattersn/multi_pointers_pattern.js
@@ -15,29 +15,32 @@
 
 //---O(n) Solution---
 
-const sumZero = (arr) => {
-    if(arr.length <= 1){
-        return undefined;
-    } else {
-        let left = 0;
-        let right = arr.length - 1;
-        while(left < right){
-            let sum = arr[left] + arr[right]
-            if(sum === 0){
-                return [arr[left], arr[right]]
-            } else if(sum > 0){
-                right--
-            } else{
-                left++
-            }
-        }
-    }
-}
+// const sumZero = (arr) => {
+//     if(arr.length <= 1){
+//         return undefined;
+//     } else {
+//         let left = 0;
+//         let right = arr.length - 1;
+//         while(left < right){
+//             let sum = arr[left] + arr[right]
+//             if(sum === 0){
+//                 return [arr[left], arr[right]]
+//             } else if(sum > 0){
+//                 right--
+//             } else{
+//                 left++
+//             }
+//         }
+//     }
+// }
 
-const arr1 = [-3, -2, -1, 0, 3] //=> [-3, 3]
-const arr2 = [-3, -1 , 0, 1, 2, 4] // => [-1, 1]
-const arr3 = [-4, -3, -2, -1, 0, 1, 2, 5] // => [-2, 2]
+// const arr1 = [-3, -2, -1, 0, 3] //=> [-3, 3]
+// const arr2 = [-3, -1 , 0, 1, 2, 4] // => [-1, 1]
+// const arr3 = [-4, -3, -2, -1, 0, 1, 2, 5] // => [-2, 2]
 
-console.log(sumZero(arr1));
-console.log(sumZero(arr2));
-console.log(sumZero(arr3));
+// console.log(sumZero(arr1));
+// console.log(sumZero(arr2));
+// console.log(sumZero(arr3));
+
+//Implement a function call countUnqiueValues, which accepts a sorted Array, and counts the unique values in the array.  Tere can be negative numbers in the array, but it will alway be sorted.
+
