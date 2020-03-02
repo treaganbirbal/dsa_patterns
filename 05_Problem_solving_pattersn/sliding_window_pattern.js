e689085303fe79b0c269cfb6bfb490b9280fb660
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 14:17:23 -0500

    start maxSubArraySum

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -1,2 +1,5 @@
 //Write a function called maxSubarraySum which accepts an array of integers and a number called n.  The function should calculate the maximum sum of n consecutive elements in the array.
 
+const maxSubarraySum = (arr, n) => {
+    return
+}
