f68804672857376362ec47650eec56bc255ff8b3
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 17:10:36 -0500

    binarySearch implementation --- O(logN) approach

--- 05_Problem_solving_pattersn/divide_and_conquer.js
+++ 05_Problem_solving_pattersn/divide_and_conquer.js
@@ -1,9 +1,29 @@
 //Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located, if the value is not found, return -1
 
-const search = (arr, searchNum) => {
-    for(let i = 0; i < arr.length; i++){
-        if(searchNum === arr[i]){
-            return i
+//----O(n) solution---
+// const search = (arr, searchNum) => {
+//     for(let i = 0; i < arr.length; i++){
+//         if(searchNum === arr[i]){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+
+const search = (arr, value) => {
+    let min = 0; 
+    let max = arr.length - 1
+    
+    while(min <= max){
+        let middle = Math.floor((min + max) / 2);
+        let currentEl = arr[middle];
+
+        if(arr[middle] < value){
+            min = middle + 1;
+        } else if( arr[middle] > value){
+            max = middle -1
+        } else {
+            return middle;
         }
     }
     return -1
