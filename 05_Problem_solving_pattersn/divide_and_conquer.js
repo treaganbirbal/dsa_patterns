e1227f5e1f064cd7fec9a4da8963eaed6931bf36
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 16:41:03 -0500

    start of search func

--- 05_Problem_solving_pattersn/divide_and_conquer.js
+++ 05_Problem_solving_pattersn/divide_and_conquer.js
@@ -1,1 +1,9 @@
 //Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located, if the value is not found, return -1
+
+const search = (arr) => {
+    return;
+}
+
+let arr1 = [1,2,3,4,5,6];
+
+// console.log(search(arr1, 4)) // => 3
