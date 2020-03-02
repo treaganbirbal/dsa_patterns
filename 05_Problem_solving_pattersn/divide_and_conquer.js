7e5eb9c5cd247dd7afb8e750df2d34ce65f8365d
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 16:39:58 -0500

    add prompt for search function

---
+++ 05_Problem_solving_pattersn/divide_and_conquer.js
@@ -0,0 +1,1 @@
+//Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located, if the value is not found, return -1
