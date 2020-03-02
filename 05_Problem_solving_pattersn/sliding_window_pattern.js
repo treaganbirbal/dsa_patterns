01c274fc3b2e4fd2bbcfb02167189908532bd475
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 14:21:02 -0500

    implement test cases

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -3,3 +3,13 @@
 const maxSubarraySum = (arr, n) => {
     return
 }
+
+let arr1 = [1, 2, 5, 2, 8, 1, 5]
+let arr2 = [4, 2, 1, 6]
+let arr3 = []
+
+console.log(maxSubarraySum(arr1, 2)); // => 10
+console.log(maxSubarraySum(arr1, 4)); // => 17
+console.log(maxSubarraySum(arr2, 1)); // => 6
+console.log(maxSubarraySum(arr2, 4)); // => 13
+console.log(maxSubarraySum(arr3, 4)); // => null
