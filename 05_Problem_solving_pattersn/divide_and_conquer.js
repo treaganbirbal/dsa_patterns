de075668aac14a85ed1a4a7a9e3f4acfd662c310
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 16:46:49 -0500

    first implementation of search func---O(n) approach

--- 05_Problem_solving_pattersn/divide_and_conquer.js
+++ 05_Problem_solving_pattersn/divide_and_conquer.js
@@ -1,7 +1,12 @@
 //Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located, if the value is not found, return -1
 
-const search = (arr) => {
-    return;
+const search = (arr, searchNum) => {
+    for(let i = 0; i < arr.length; i++){
+        if(searchNum === arr[i]){
+            return i
+        }
+    }
+    return -1
 }
 
 let arr1 = [1,2,3,4,5,6];
