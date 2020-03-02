8c5c53230c9ac68700aa34bad9b208a4a9411913
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 17:48:58 -0500

    implement test cases for sameFrequency

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -117,3 +117,7 @@ function sameFrequency(int1, int2){
     return;
 }
 
+// console.log(sameFrequency(182, 281)) // => true;
+// console.log(sameFrequency(34, 14)) // => false;
+// console.log(sameFrequency(3589578, 5879385)) // => true;
+// console.log(sameFrequency(22, 222)) // => false;
