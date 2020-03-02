7f1168b01d408a82e54f978152d938721029a673
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 14:15:31 -0500

    prompt for maxSubArraySum

---
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -0,0 +1,2 @@
+//Write a function called maxSubarraySum which accepts an array of integers and a number called n.  The function should calculate the maximum sum of n consecutive elements in the array.
+
