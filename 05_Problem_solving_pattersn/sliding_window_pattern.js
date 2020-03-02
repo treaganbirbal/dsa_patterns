88fca332c3eb1be3cedcbc746ef3b867cbd822ff
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 15:52:48 -0500

    start of O(n) solution

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -1,20 +1,28 @@
 //Write a function called maxSubarraySum which accepts an array of integers and a number called n.  The function should calculate the maximum sum of n consecutive elements in the array.
 
-const maxSubarraySum = (arr, num) => {
-    if(arr.length < num){
+//----O(n^2) solution----
+// const maxSubarraySum = (arr, num) => {
+//     if(arr.length < num){
+//         return null;
+//     }
+//     let max = -Infinity;
+//     for(let i = 0; i < arr.length - num + 1; i++){
+//         let temp = 0;
+//         for(let j = 0; j < num; j++){
+//             temp += arr[i + j];
+//         }
+//         if(temp > max){
+//             max = temp;
+//         }
+//     }
+//     return max
+// }
+
+function maxSubarraySum(arr, num){
+    if(num > arr.lengt){
         return null;
     }
-    let max = -Infinity;
-    for(let i = 0; i < arr.length - num + 1; i++){
-        let temp = 0;
-        for(let j = 0; j < num; j++){
-            temp += arr[i + j];
-        }
-        if(temp > max){
-            max = temp;
-        }
-    }
-    return max
+    let max
 }
 
 let arr1 = [1, 2, 5, 2, 8, 1, 5]
