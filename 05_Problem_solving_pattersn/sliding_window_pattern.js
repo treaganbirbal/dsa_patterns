fe27e4c9be51a5572ceb7e48a48af2a0dfe225ce
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 15:59:47 -0500

    implmentation of maxSubarraySum O(n) solution

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -19,10 +19,20 @@
 // }
 
 function maxSubarraySum(arr, num){
-    if(num > arr.lengt){
+    if(num > arr.length){
         return null;
     }
-    let max
+    let maxSum = 0; 
+    let tempSum = 0;
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum)
+    }
+    return maxSum;
 }
 
 let arr1 = [1, 2, 5, 2, 8, 1, 5]
