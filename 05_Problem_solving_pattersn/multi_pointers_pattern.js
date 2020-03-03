330472630aa6533bc3838919b6e0ca6ab7081e8b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 20:31:17 -0500

    test cases for areThereDuplicates

--- 05_Problem_solving_pattersn/multi_pointers_pattern.js
+++ 05_Problem_solving_pattersn/multi_pointers_pattern.js
@@ -69,3 +69,11 @@
 // console.log(countUnqiueValues(arr4)) // => 4
 
 // Implement a function called areThereDuplicates, which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. 
+
+const areThereDuplicates = (...args) => {
+    console.log(args)
+}
+
+console.log(areThereDuplicates(1, 2, 3)) //=> false;
+console.log(areThereDuplicates(1, 2, 2)) //=> true;
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) // => true;
