0cd5cc364235fc24b90a44bf976cf15373b2ca3a
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 20:28:01 -0500

    implementation of areThereDuplicates

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -144,6 +144,19 @@
 
 // Implement a function called areThereDuplicates, which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. 
 
+function areThereDuplicates(){
+    const collection = {};
+    for(let values in arguments){
+        collection[arguments[values]] = (collection[arguments[values]] || 0) + 1
+    }
+    for(let key in collection){
+        if(collection[key] > 1){
+            return true;
+        }
+    }
+    return false
+}
+
 console.log(areThereDuplicates(1, 2, 3)) //=> false;
 console.log(areThereDuplicates(1, 2, 2)) //=> true;
 console.log(areThereDuplicates('a', 'b', 'c', 'a')) // => true;
