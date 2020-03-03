b7bd5aa81cc06d53c537fc2090988a9759755dd1
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-03 18:32:43 -0500

    Prompt for maxSubarraySum problem

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -18,29 +18,32 @@
 //     return max
 // }
 
-function maxSubarraySum(arr, num){
-    if(num > arr.length){
-        return null;
-    }
-    let maxSum = 0; 
-    let tempSum = 0;
-    for(let i = 0; i < num; i++){
-        maxSum += arr[i];
-    }
-    tempSum = maxSum;
-    for(let i = num; i < arr.length; i++){
-        tempSum = tempSum - arr[i - num] + arr[i];
-        maxSum = Math.max(maxSum, tempSum)
-    }
-    return maxSum;
-}
+// function maxSubarraySum(arr, num){
+//     if(num > arr.length){
+//         return null;
+//     }
+//     let maxSum = 0; 
+//     let tempSum = 0;
+//     for(let i = 0; i < num; i++){
+//         maxSum += arr[i];
+//     }
+//     tempSum = maxSum;
+//     for(let i = num; i < arr.length; i++){
+//         tempSum = tempSum - arr[i - num] + arr[i];
+//         maxSum = Math.max(maxSum, tempSum)
+//     }
+//     return maxSum;
+// }
 
-let arr1 = [1, 2, 5, 2, 8, 1, 5]
-let arr2 = [4, 2, 1, 6]
-let arr3 = []
+// let arr1 = [1, 2, 5, 2, 8, 1, 5]
+// let arr2 = [4, 2, 1, 6]
+// let arr3 = []
 
-console.log(maxSubarraySum(arr1, 2)); // => 10
-console.log(maxSubarraySum(arr1, 4)); // => 17
-console.log(maxSubarraySum(arr2, 1)); // => 6
-console.log(maxSubarraySum(arr2, 4)); // => 13
-console.log(maxSubarraySum(arr3, 4)); // => null
+// console.log(maxSubarraySum(arr1, 2)); // => 10
+// console.log(maxSubarraySum(arr1, 4)); // => 17
+// console.log(maxSubarraySum(arr2, 1)); // => 6
+// console.log(maxSubarraySum(arr2, 4)); // => 13
+// console.log(maxSubarraySum(arr3, 4)); // => null
+
+//Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sun of a subarray with the length of the number passed to the function.
+//Note: that a subarray must consist of consecutive elements from the origin arr.
