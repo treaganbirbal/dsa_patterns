a035324fc75934d2420517c3190803f6a5d8369f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 20:48:40 -0500

    areThereDuplicates solution using sets

--- 05_Problem_solving_pattersn/multi_pointers_pattern.js
+++ 05_Problem_solving_pattersn/multi_pointers_pattern.js
@@ -70,9 +70,24 @@
 
 // Implement a function called areThereDuplicates, which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. 
 
-const areThereDuplicates = (...args) => {
-    console.log(args)
-}
+// const areThereDuplicates = (...args) => {
+//     args.sort((a, b) => a > b)
+//     let start = 0;
+//     let next = 1;
+//     while(next < args.length){
+//         if(args[start] === args[next]){
+//             return true;
+//         }
+//         start++
+//         next++
+//     }
+//     return false;
+// }
+
+function areThereDuplicates() {
+    return new Set(arguments).size !== arguments.length;
+  }
+  
 
 console.log(areThereDuplicates(1, 2, 3)) //=> false;
 console.log(areThereDuplicates(1, 2, 2)) //=> true;
