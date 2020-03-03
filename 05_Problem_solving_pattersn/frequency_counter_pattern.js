faa3de2e1b30b0ee966116dfab55918eab1e8a5b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-02 20:08:20 -0500

    create test cases for areThereDuplicates

--- 05_Problem_solving_pattersn/frequency_counter_pattern.js
+++ 05_Problem_solving_pattersn/frequency_counter_pattern.js
@@ -143,3 +143,7 @@
 // console.log(sameFrequency(22, 222)) // => false;
 
 // Implement a function called areThereDuplicates, which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. 
+
+console.log(areThereDuplicates(1, 2, 3)) //=> false;
+console.log(areThereDuplicates(1, 2, 2)) //=> true;
+console.log(areThereDuplicates('a', 'b', 'c', 'a')) // => true;
