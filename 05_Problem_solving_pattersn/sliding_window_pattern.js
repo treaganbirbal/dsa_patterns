3ba47f46280ab82432499f60098f624c8dbd7dd0
Treagan Birbal <treaganbirbal@pursuit.org>
2020-03-04 11:06:59 -0500

    implementation of minSubArrayLen

--- 05_Problem_solving_pattersn/sliding_window_pattern.js
+++ 05_Problem_solving_pattersn/sliding_window_pattern.js
@@ -80,13 +80,34 @@
 //Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.  The function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function.  If there is none ,return 0.
 
 const minSubArrayLen = (arr, num) => {
-    return 
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+    while(start < arr.length){
+        //if current window doesn't add up to the given sum then move the window to right
+        if(total < num && end < arr.length){
+            total += arr[end]
+            end++
+        }
+        //if current window adds up to at least the sum given then we can shrink the the window
+        else if(total >= num){
+            minLen = Math.min(minLen, end-start)
+            total -= arr[start]
+            start++
+        }
+        //current total less than require but we reach the end
+        else {
+            break;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen
 }
 
 let arr1 = [2, 3, 1, 2, 4, 4]
 let arr2 = [2, 1, 6, 5, 4]
 let arr3 = [3,1,7,11,2,9,8,21,62,33,19]
-let arr4 = [1,4,16,2,,5,6,7,8,10]
+let arr4 = [1,4,16,22,5,7,8, 9,10]
 let arr5 = [4, 3, 3, 8, 1, 2, 3]
 
 console.log(minSubArrayLen(arr1, 7)) // => 2
