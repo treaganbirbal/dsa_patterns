faaabcb5e6d8e13a991b19903c296c92294826cc
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-07 22:16:06 -0400

    O(n) solution for countUniqueValues practice problem

--- 05_Problem_solving_pattersn/multi_pointers_pattern.js
+++ 05_Problem_solving_pattersn/multi_pointers_pattern.js
@@ -16,56 +16,56 @@
 
 //---O(n) Solution---
 
-const sumZero = (arr) => {
-  if (!arr.length) {
-    return undefined;
-  }
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-    if (arr[left] + arr[right] > 0) {
-      right--;
-    } else if (arr[left] + arr[right] < 0) {
-      left++;
-    } else if (arr[left] + arr[right] === 0) {
-      return [arr[left], arr[right]];
-    }
-  }
-};
+// const sumZero = (arr) => {
+//   if (!arr.length) {
+//     return undefined;
+//   }
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     if (arr[left] + arr[right] > 0) {
+//       right--;
+//     } else if (arr[left] + arr[right] < 0) {
+//       left++;
+//     } else if (arr[left] + arr[right] === 0) {
+//       return [arr[left], arr[right]];
+//     }
+//   }
+// };
 
-const arr1 = [-3, -2, -1, 0, 3]; //=> [-3, 3]
-const arr2 = [-3, -1, 0, 1, 2, 4]; // => [-1, 1]
-const arr3 = [-4, -3, -2, -1, 0, 1, 2, 5]; // => [-2, 2]
+// const arr1 = [-3, -2, -1, 0, 3]; //=> [-3, 3]
+// const arr2 = [-3, -1, 0, 1, 2, 4]; // => [-1, 1]
+// const arr3 = [-4, -3, -2, -1, 0, 1, 2, 5]; // => [-2, 2]
 
-console.log(sumZero(arr1));
-console.log(sumZero(arr2));
-console.log(sumZero(arr3));
+// console.log(sumZero(arr1));
+// console.log(sumZero(arr2));
+// console.log(sumZero(arr3));
 
 //Implement a function call countUnqiueValues, which accepts a sorted Array, and counts the unique values in the array.  Tere can be negative numbers in the array, but it will alway be sorted.
 
-// function countUnqiueValues(arr){
-//     if(!arr.length){
-//         return 0;
-//     }
-//     let i = 0;
-//     for(let j = 1; j < arr.length; j++){
-//         if(arr[i] !== arr[j]){
-//             i++;
-//             arr[i] = arr[j];
-//         }
-//     }
-//     return i+1
-// }
+const countUnqiueValues = (arr) => {
+  if (!arr.length) {
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
 
-// let arr1 = [1,1,1,1,1,1,2];
-// let arr2 = [1,2,3,4,4,4,5,5,12,12,13];
-// let arr3 = [];
-// let arr4 = [-2, -1, -1, 0, 1];
+let arr1 = [1, 1, 1, 1, 1, 1, 2];
+let arr2 = [1, 2, 3, 4, 4, 4, 5, 5, 12, 12, 13];
+let arr3 = [];
+let arr4 = [-2, -1, -1, 0, 1];
 
-// console.log(countUnqiueValues(arr1)) // => 2
-// console.log(countUnqiueValues(arr2)) // => 7
-// console.log(countUnqiueValues(arr3)) // => 0
-// console.log(countUnqiueValues(arr4)) // => 4
+console.log(countUnqiueValues(arr1)); // => 2
+console.log(countUnqiueValues(arr2)); // => 7
+console.log(countUnqiueValues(arr3)); // => 0
+console.log(countUnqiueValues(arr4)); // => 4
 
 // Implement a function called areThereDuplicates, which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.
 
