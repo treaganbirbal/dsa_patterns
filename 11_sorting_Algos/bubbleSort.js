cb97198577ea8d3cfe53815de90e679a5b1b1b4a
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-11 19:57:32 -0400

    implementation of bubbleSort with optimization

--- 11_sorting_Algos/bubbleSort.js
+++ 11_sorting_Algos/bubbleSort.js
@@ -2,13 +2,31 @@
 
 //define a function called bubbleSort that takes in an array of numbers and sorts them in place
 
-function bubbleSort(arr) {}
+function bubbleSort(arr) {
+  //check if swap has been done to avoid extra passes
+  let noSwap;
+  //loop from array with a variable i  from end of array towards beginning
+  for (let i = arr.length - 1; i >= 0; i--) {
+    noSwap = true;
+    //loop with the variable j from the beginning of array until i - 1
+    for (let j = 0; j <= i - 1; j++) {
+      //if arr[j] is greater than arr[j + 1], swap those two values
+      if (arr[j] > arr[j + 1]) {
+        noSwap = false;
+        swap(arr, j, j + 1);
+      }
+    }
+    if (noSwap) break;
+  }
+  //return the sorted array.
+  return arr;
+}
 
 function swap(arr, index1, index2) {
   let temp = arr[index1];
   arr[index1] = arr[index2];
   arr[index2] = temp;
-  return arr
+  return arr;
 }
-
-console.log(swap([1, 2], 0, 1));
+let array = [6345, 2, 37, 19, 44, 2, 4, 18, 1];
+console.log(bubbleSort(array));
