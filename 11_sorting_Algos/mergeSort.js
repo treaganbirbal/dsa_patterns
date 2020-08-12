47bacb6e5ffc81cd7750426ee1065491d94079fe
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-12 13:09:28 -0400

    implementation of merge function part of mergeSort

---
+++ 11_sorting_Algos/mergeSort.js
@@ -0,0 +1,38 @@
+//Merge sort is a combination of three things - splitting, merging and sorting
+//exploits the act that arrays of 0 or 1 elements are always sorted
+//Works by decomposing an array into smaller arrays of 0 or 1 elements and then building up a newly sorted array
+
+function mergeSort() {}
+//given two sorted arrays this helper function should create a new array which is also sorted and consists of all the elements in the two input arrays
+//time: O(n + m)  space: O(n+m)
+function merge(arr1, arr2) {
+  //create an empty array to return
+  const output = [];
+  //look at the smallest values in each input array
+  let i = 0;
+  let j = 0;
+  //while there are still values to look at.....
+  while (i < arr1.length && j < arr2.length) {
+    //if the value in the first array is smaller than the value in the second array,push the value in the first array into output array and move on to the next value in the first array
+    if (arr2[j] > arr1[i]) {
+      output.push(arr1[i]);
+      i++;
+    } else {
+      //if the value in the larger than the value in the second array, push the value in the second array into our output array and move on to the next value in the second array
+      output.push(arr2[j]);
+      j++;
+    }
+  }
+  //once we exhause one array, push in all remaining values from the other array
+  while (i < arr1.length) {
+    output.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    output.push(arr2[j]);
+    j++;
+  }
+  return output;
+}
+
+console.log(merge([3, 5, 8, 10], [1, 2, 4, 6, 9]));
