16d9232deeb3420578bede712096bd4215834c70
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-15 14:44:10 -0400

    implmentation of push method

--- 19_Singly_Linked_List/singly_linkedList.js
+++ 19_Singly_Linked_List/singly_linkedList.js
@@ -13,6 +13,27 @@ class SinglyLinkedList {
   }
 
   //push method adds new node to list
+  push(val) {
+    //create new Node with value
+    //if no head
+    //set head & tail to new node
+    //else set next property on tail to be new node and set the tail property on on the list to be newly created node
+    //update length property by one
+    //return list
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
+list.push(1).push(2).push(3);
+console.log(list);
