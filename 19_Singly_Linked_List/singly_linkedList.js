b424487f1ce1178f7f16fd4dc21c9b4c3db1458b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-15 14:36:15 -0400

    initialize Node class & LL class for SinglyLinkedList ds

---
+++ 19_Singly_Linked_List/singly_linkedList.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  //push method adds new node to list
+}
+
+let list = new SinglyLinkedList();
