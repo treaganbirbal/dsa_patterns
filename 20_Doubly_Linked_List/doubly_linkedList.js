1f6ad146123325e0f55f9efbb73ba4b376f432f5
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-16 17:28:35 -0400

    bug fix: implementation of pop metho

--- 20_Doubly_Linked_List/doubly_linkedList.js
+++ 20_Doubly_Linked_List/doubly_linkedList.js
@@ -38,12 +38,27 @@ class DoublyLinkedList {
 
   //remove a node from the end of the linked list
   pop() {
+    //if no tail
+    //return undefined
+    //else store current tail in variable
+    //if length is 1
+    //set head & tail to be null
+    //update the tail to be prev Node
+    //set new tail next to be null
+    //decrement length by 1
+    //return the removed node
     if (!this.tail) {
       return undefined;
     }
     let lastNode = this.tail;
-    this.tail = lastNode.prev;
-    this.tail.next = null;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = lastNode.prev;
+      this.tail.next = null;
+      lastNode.prev = null;
+    }
     this.length--;
     return lastNode;
   }
@@ -69,4 +84,6 @@ const list = new DoublyLinkedList();
 list.push(1).push(2).push(3);
 console.log(list);
 console.log("removed Node:", list.pop());
+list.pop();
+list.pop();
 console.log(list);
