27bd01ae31434c53ee3e7f7e6bd049f1bd4ee25b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-25 16:42:41 -0400

    naive implementation of insert method

--- 22_Binary_search_trees/bst.js
+++ 22_Binary_search_trees/bst.js
@@ -16,4 +16,52 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  //inserting a node at the appropiate place
+  insert(value) {
+    //create new node that takes in a val
+    //start at root, if no root new Node becomes root
+    //if there is a root, check the value of the new node is greater or less than value of root
+    //if it is greater
+    //check to see if there is a node to the right
+    //if ther is move on to the next node and repeat these steps
+    //if there is no node, add that node as the right property
+    //else if it is less
+    //check to see if there is a node to the right
+    //if there is a node, move to left of that node and repeat these steps
+    //if there is no node, add that node as the left property
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+    return this;
+  }
 }
+
+const bst = new BinarySearchTree();
+console.log(bst);
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(12);
+console.log(bst.root.right);
