1eaab72ece6596d36ca5f5cb8a6a8573c1bc3480
Treagan Birbal <treaganbirbal@pursuit.org>
2020-08-29 22:45:03 -0400

    implementation of extractMax method

--- 24_Priority_heaps/priority_heaps.js
+++ 24_Priority_heaps/priority_heaps.js
@@ -31,6 +31,57 @@ class MaxBinaryHeap {
     this.bubble();
   }
 
+  //removing a node from a heap
+  extractMax() {
+    //Swap the first value in the values property with the last one
+    //Pop from the values property, so you can return the value at the end.
+    //Have the new root sink down to the correct spot
+    //Your parent index starts at 0 (this root)
+    //Find the index of the left child 2* index + 1 *make sure ites not out of bounds
+    //find the index of the right child 2*index + 2 make sure its not out of bounds
+    //If the left or right child is greater than the element, swap. If both left and right child are larger, swap with the largest child.
+    //the child index you swapped to now becomes the new parent index
+    //keep looping and swapping until neither child is larger than element.
+    //Return the old root
+    let maxVal = this.values[0];
+    let lastElement = this.values.pop();
+    this.values[0] = lastElement;
+    this.sinkDown();
+    return maxVal;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+
   bubble() {
     let childIdx = this.values.length - 1;
     const child = this.values[childIdx];
@@ -52,4 +103,7 @@ maxBH.insert(4);
 maxBH.insert(33);
 maxBH.insert(2);
 maxBH.insert(99);
+console.log(maxBH);
+console.log(maxBH.extractMax());
+maxBH.extractMax();
 console.log(maxBH);
